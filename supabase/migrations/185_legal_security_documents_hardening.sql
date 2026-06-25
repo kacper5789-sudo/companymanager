b6ff8c46ba03199c82c73b94961e22ceefbf03a6
Kacper Piskorz <kacper5789@gmail.com>
2026-06-25 11:19:47 +0200

    update

---
+++ supabase/migrations/185_legal_security_documents_hardening.sql
@@ -0,0 +1,4 @@
+-- 185_legal_security_documents_hardening
+-- Informacyjna migracja: dokumenty publiczne zostały utwardzone prawnie w HTML.
+-- Zakres: regulamin.html, polityka-prywatnosci.html, informacja-o-przetwarzaniu-danych.html.
+select '185 legal security documents hardening: no database changes' as info;
