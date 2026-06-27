90bf70f0d042e1e07fb36a9fa2eadf12ef3ea577
Kacper Piskorz <kacper5789@gmail.com>
2026-06-27 08:38:06 +0200

    u

---
+++ 251_gold_white_skin_premium_polish.sql
@@ -0,0 +1,13 @@
+-- CompanyManager 251_gold_white_skin_premium_polish.sql
+-- Informacyjny checkpoint UI.
+-- Brak migracji bazy danych.
+-- Zakres: dopracowanie wyłącznie skórki Złoto / biały:
+-- - złote gradienty,
+-- - glass surfaces,
+-- - czarne/czytelne teksty zamiast niebieskich/białych pozostałości,
+-- - jasne formularze i panele firmy,
+-- - jasny grafik pracy,
+-- - czytelniejsze wykresy/statystyki,
+-- - hover animations.
+select '251_gold_white_skin_premium_polish' as patch,
+       'ui_only_no_database_changes' as scope;
